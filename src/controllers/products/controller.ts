91206207be6948541d364bf0b11f87448f7cd43f
shaharsol <shahar@gnift.com>
2024-01-24 14:26:43 +0200

    better joi

--- src/controllers/products/controller.ts
+++ src/controllers/products/controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import productModel from '../../models/product/mysql'
 import getModel from '../../models/product/factory'
+import DTO from '../../models/product/dto';
 
 export async function getAll(req: Request, res: Response) {
     const products = await getModel().getAll();
@@ -8,6 +9,6 @@ export async function getAll(req: Request, res: Response) {
 }
 
 export async function insert(req: Request, res: Response) {
-    const newProduct = await getModel().insert(req.body);
+    const newProduct = await getModel().insert(req.body as DTO);
     res.json(newProduct)
 }
--- src/controllers/products/validator.ts
+++ src/controllers/products/validator.ts
@@ -1,6 +1,7 @@
 import joi from 'joi';
+import DTO from '../../models/product/dto';
 
-export const newProductValidator = joi.object({
+export const newProductValidator = joi.object<DTO>({
     name: joi.string().required(),
     price: joi.number().required(),
     stock: joi.number().required()
