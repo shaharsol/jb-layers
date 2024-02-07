49163560ba46712584a55b96177cbc66d60c64ba
shaharsol <shahar@gnift.com>
2024-02-07 18:20:59 +0200

    better test

--- src/routes/products.test.ts
+++ src/routes/products.test.ts
@@ -10,6 +10,12 @@ describe('test products router', () => {
         it('should return an array of products', async () => {
             const res = await request(server).get('/products')
             expect(res.statusCode).toBe(200);
+            expect(Array.isArray(res.body)).toBeTruthy
+            expect(res.body[0]).toHaveProperty('id');
+            expect(res.body[0]).toHaveProperty('name');
+            expect(res.body[0]).toHaveProperty('price');
+            expect(res.body[0]).toHaveProperty('stock');
+            expect(res.body[0]).toHaveProperty('imageName');
         })
     })
 })
