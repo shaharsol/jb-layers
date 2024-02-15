e75064e0298726bc8de3e5d29a0b3719c0f0c7a7
shaharsol <shahar@gnift.com>
2024-02-15 23:43:33 +0200

    mock query

--- src/routes/products.test.ts
+++ src/routes/products.test.ts
@@ -22,14 +22,25 @@ describe('test products router', () => {
     // unit test
     describe('test /api/products/1 endpoint', () => {
         it('should return single product with id=1', async () => {
-            const res = await request(server).get('/api/products')
+            jest.mock('../db/mysql', () => {
+                return {
+                    default: jest.fn(() => [{
+                        id: 1,
+                        name: 'chai',
+                        price: 18,
+                        stock: 6,
+                        imageName: 'a'
+                    }])
+                }
+            })
+            const res = await request(server).get('/api/products/1')
             expect(res.statusCode).toBe(200)
-            expect(Array.isArray(res.body)).toBeTruthy()
-            expect(res.body[0]).toHaveProperty('id')
-            expect(res.body[0]).toHaveProperty('name')
-            expect(res.body[0]).toHaveProperty('price')
-            expect(res.body[0]).toHaveProperty('stock')
-            expect(res.body[0]).toHaveProperty('imageName')
+            expect(res.body).toHaveProperty('id')
+            expect(res.body.id).toBe(1)
+            expect(res.body).toHaveProperty('name')
+            expect(res.body).toHaveProperty('price')
+            expect(res.body).toHaveProperty('stock')
+            expect(res.body).toHaveProperty('imageName')
         })
     })
 })
